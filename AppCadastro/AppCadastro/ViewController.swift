28a5f814015be4250f86b9d73aeacb25f0a6f9c6
Amanda Nolasco <nolasco.amanda@outlook.com>
2020-08-13 19:43:01 -0300

    criei e apliquei os elementos da primeira tela

---
+++ AppCadastro/AppCadastro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppCadastro
+//
+//  Created by Adriel Alves on 11/08/20.
+//  Copyright © 2020 DH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var cadastrarButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        
+    }
+
+
+}
+
